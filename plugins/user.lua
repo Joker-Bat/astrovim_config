90a6f79d25e894ec7e8826531820dd707da1db23
Joker-Bat <shanmugam091098@gmail.com>
2024-03-04 19:36:20 +0530

    Wakatime plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,7 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+    setup = function() vim.cmd [[packadd wakatime/vim-wakatime]] end,
+  },
 }
